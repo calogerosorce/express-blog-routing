66b1d62cce706468282f111fa4f69271112402cb
calogerosorce <calogerosorce00@hotmail.com>
2025-11-25 12:07:24 +0100

    Add posts routing functionality with CRUD operations

---
+++ routers/posts_rout.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    res.send('Show all post here')
+})
+
+router.get('/:id', (req, res) => {
+    res.send(`Show all post whith id:${req.params.id}`)
+})
+
+router.post('/', (req, res) => {
+    res.send('Store a new post here')
+})
+
+router.put('/:id', (req, res) => {
+    res.send(`Update post whith ${req.params.id}`)
+})
+
+router.patch('/:id', (req, res) => {
+    res.send(`Modify the post whith ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+    res.send(`Delete the post whith ${req.params.id}`)
+})
+
+
+module.exports = router
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const postsRouter = require('./routers/posts_rout')
 
 app.use(express.static('public'))
 
@@ -11,3 +12,5 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send('<h1>Server del mio blog</h1>')
 })
+
+app.use('/posts', postsRouter)
